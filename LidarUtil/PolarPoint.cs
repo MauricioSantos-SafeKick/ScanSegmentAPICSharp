58b423b4bc249d4fa90d678bd9e909ee9a979074
Mauricio Santos <Mauricio.Santos@safekick.com>
2025-06-24 15:34:53 -0500

    Introduce Util Library, add polar and xy points.

---
+++ LidarUtil/PolarPoint.cs
@@ -0,0 +1,22 @@
+﻿namespace LidarUtil;
+
+internal class PolarPoint
+{
+  public double R { get; set; }
+  /// <summary>
+  /// radians
+  /// </summary>
+  public double Theta { get; set; }
+
+  public PolarPoint(double r, double theta)
+  {
+    if (r < 0)
+      throw new("R must be greater than zero.");
+
+    if (theta < -2 * Math.PI || theta > 2 * Math.PI)
+      throw new("Theta must be between -2π and 2π.");
+
+    R = r;
+    Theta = theta;
+  }
+}
---
+++ LidarUtil/XYPoint.cs
@@ -0,0 +1,26 @@
+﻿using static System.Math;
+
+namespace LidarUtil;
+
+public class XyPoint
+{
+    // create x and y points
+    public double X { get; set; }
+    public double Y { get; set; }
+
+    public XyPoint(double x, double y)
+    {
+      X = x;
+      Y = y;
+    }
+
+    public static XyPoint FromPolar(double r, double theta)
+    {
+      var x = r * Cos(theta);
+      var y = r * Sin(theta);
+
+      return new(x, y);
+    }
+}
+
+
